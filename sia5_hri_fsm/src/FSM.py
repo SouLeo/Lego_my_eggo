2f5bee053b439a5d6953b7cfcd425c9b7be5e714
SouLeo <slwanna@utexas.edu>
2017-12-05 12:48:48 -0600

    updated some style

--- sia5_hri_fsm/src/FSM.py
+++ sia5_hri_fsm/src/FSM.py
@@ -17,8 +17,10 @@ possiblePatterns = []
 
 class Observe(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['examine_puzzle'],
-                             input_keys=['is_block_placed_in'], output_keys=[''])
+        smach.State.__init__(self,
+                             outcomes=['examine_puzzle'],
+                             input_keys=['is_block_placed_in'],
+                             output_keys=[''])
 
     def execute(self, userdata):
         rospy.loginfo('Executing Observation State')
@@ -33,7 +35,8 @@ class Observe(smach.State):
 
 class ExaminePuzzle(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['give_next_block'],
+        smach.State.__init__(self,
+                             outcomes=['give_next_block'],
                              input_keys=['is_blocked_placed_out', 'is_pattern_known'],
                              output_keys=['next_block'])
 
@@ -68,7 +71,9 @@ class GiveBlock(smach.State):
     # this may turn into a service state type or an action state type. Talk w/
     # Christina
     def __init__(self):
-        smach.State.__init__(self, outcomes=['observe'], input_keys=['next_block'],
+        smach.State.__init__(self,
+                             outcomes=['observe'],
+                             input_keys=['next_block'],
                              output_keys=['is_block_placed_in'])
 
     def execute(self, userdata):
@@ -80,6 +85,7 @@ class GiveBlock(smach.State):
             # Place block
             # TODO: rosservice call to place a the next block in the drop off zone
             print 'lol'
+        # TODO: Visually check for updated puzzle
         return 'observe'
 
 def is_block_placed_cb(data):

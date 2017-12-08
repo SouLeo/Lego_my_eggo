d863b310510d980a5aeeadb9d338fae496594648
SouLeo <slwanna@utexas.edu>
2017-12-08 15:05:29 -0600

    srv files should build

---
+++ sia5_hri_fsm/src/FSM.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+"""
+    This code contains the main function to run the SIA-5 finite state
+    machine for Dr. Thomaz's HRI Project.
+
+    The state machine is comprised of three states:
+    1. Observe
+    2. ExaminePuzzle
+    3. GiveBlock
+
+    Maintainer: Selma Wanna, slwanna@utexas.edu
+"""
+
+import rospy
+import smach
+import smach_ros
+from Puzzle import (create_patterns, create_starter_patterns,
+                    compare_pattern, next_block, is_piece_available)
+from Block import get_pose
+from std_msgs.msg import Bool
+from sia5_hri_fsm.srv import Handover
+
+SM = None
+TIMEOUT_SECS = 30
+RAND = True
+NUMERRGUESS = 0
+POSSIBLE_PATTERNS = []
+STARTER_PATTERNS = ''
+
+class Observe(smach.State):
+    """
+        The Observation state checks to see if a user has filled in a
+        new block. it will timeout in 30 seconds if no block is placed.
+        Regardless of timeout it will enter the state ExaminePuzzle
+    """
+
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['examine_puzzle'],
+                             input_keys=['is_block_placed_in'],
+                             output_keys=[''])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing Observation State')
+        timeout = rospy.Time.now() + rospy.Duration(TIMEOUT_SECS)
+        while not userdata.is_block_placed_in and rospy.Time.now() <= timeout:
+            pass
+        if not userdata.is_block_placed_in:
+            rospy.loginfo('Timeout Occurred')
+        else:
+            rospy.loginfo('Block in place')
+        return 'examine_puzzle'
+
+class ExaminePuzzle(smach.State):
+    """
+        The ExaminePuzzle state decodes the camera image into a string
+        and refines the possible patterns the human is constructing.
+        It will provide the next block to handover to the human
+    """
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['give_next_block'],
+                             input_keys=['is_blocked_placed_out', 'is_pattern_known'],
+                             output_keys=['next_block'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing Examine Puzzle State')
+        global POSSIBLE_PATTERNS
+        global NUMERRGUESS
+        # TODO: rosservice call that reports block sequence into a string and
+        # report into current_pattern
+        current_pattern = 'rr'   # use rosservice that determines what blocks are shown
+        if not userdata.is_pattern_known:
+            POSSIBLE_PATTERNS = compare_pattern(current_pattern, POSSIBLE_PATTERNS)
+            num_solns = len(POSSIBLE_PATTERNS)
+            if num_solns == 1:
+                rospy.loginfo('Pattern Discovered!')
+                userdata.is_pattern_known = True
+                new_piece = next_block(RAND, POSSIBLE_PATTERNS, current_pattern) # next likely block
+            else:
+                rospy.loginfo('Pattern still unknown. ' + str(num_solns) + ' possibilities')
+                NUMERRGUESS = NUMERRGUESS + 1
+                new_piece = next_block(RAND, POSSIBLE_PATTERNS, current_pattern, NUMERRGUESS)
+        else:
+            new_piece = next_block(RAND, POSSIBLE_PATTERNS, current_pattern) # next likely block
+            rospy.loginfo('Pattern is known!')
+        userdata.next_block = new_piece
+        rospy.loginfo('Giving Block ' + new_piece)
+        return 'give_next_block'
+
+
+class GiveBlock(smach.State):
+    """
+        The GiveBlock state provides the needed block (determined by the
+        ExaminePuzzle state) to the shared human-robot shared workspace
+    """
+    # TODO: this state involves placing a block in the handover zone, the robot,
+    # this may turn into a service state type or an action state type. Talk w/
+    # Christina
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['observe'],
+                             input_keys=['next_block'],
+                             output_keys=['is_block_placed_in'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing Give Block State')
+        handover_block = is_piece_available(userdata.next_block, STARTER_PATTERNS)
+        if not handover_block:
+            # TODO: Don't hand anything over, the user has his/her block
+            rospy.loginfo('No action taken')
+            # remove color from global pattern
+        else:
+            # Place block
+            # TODO: rosservice call to place a the next block in the drop off zone
+            block_pose = get_pose(userdata.next_block)
+            rospy.ServiceProxy('handover', block_pose)
+            rospy.loginfo('Pose is ' + str(block_pose.pose.position.x) + ', '
+                          + str(block_pose.pose.position.y))
+
+        # TODO: Visually check for updated puzzle
+        return 'observe'
+
+def is_block_placed_cb(data):
+    """
+        Function:    is_block_placed_cb
+        Input:       block placed? (boolean)
+        Output:      none
+    """
+    rospy.loginfo('block has been placed!')
+    SM.userdata.sm_is_block_placed = data.data
+
+def main():
+    """
+        Initializes and runs the state machine for the SIA-5.
+        State machine is viewable with
+        rosrun smach_viewer smach_viewer.py
+    """
+    # Set up ROS functionality
+    rospy.init_node('sia5_fsm')
+    # ROS publisher and subscribers
+    rospy.Subscriber('is_block_placed', Bool, is_block_placed_cb)
+
+    # State Machine Setup
+    global SM
+    SM = smach.StateMachine(outcomes=[''])
+    rospy.loginfo('sm defined')
+
+    # Initialize state machine variables
+    SM.userdata.sm_is_block_placed = False
+    SM.userdata.sm_is_pattern_known = False
+    SM.userdata.sm_next_block = ''
+
+    # Pattern Globals
+    global POSSIBLE_PATTERNS
+    POSSIBLE_PATTERNS = create_patterns()
+
+    global STARTER_PATTERNS
+    STARTER_PATTERNS = create_starter_patterns()
+
+    with SM:
+        # Add states to container
+        smach.StateMachine.add('OBSERVE', Observe(),
+                               transitions={'examine_puzzle':'EXAMINEPUZZLE'},
+                               remapping={'is_block_placed_in':'sm_is_block_placed'})
+        smach.StateMachine.add('EXAMINEPUZZLE', ExaminePuzzle(),
+                               transitions={'give_next_block':'GIVEBLOCK'},
+                               remapping={'is_block_placed_out':'sm_is_block_placed',
+                                          'is_pattern_known':'sm_is_pattern_known',
+                                          'next_block':'sm_next_block'})
+        smach.StateMachine.add('GIVEBLOCK', GiveBlock(),
+                               transitions={'observe':'OBSERVE'},
+                               remapping={'is_pattern_known':'sm_is_pattern_known',
+                                          'next_block':'sm_next_block',
+                                          'is_block_placed_in':'sm_is_block_placed'})
+
+    sis = smach_ros.IntrospectionServer('sia5_fsm', SM, '/sm_ROOT')
+    sis.start()
+
+    SM.execute()
+    rospy.spin()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
---
+++ sia5_hri_fsm/src/Puzzle.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+"""
+    This code contains the puzzle helper functions to run the
+    SIA-5 finite state machine for Dr. Thomaz's HRI Project.
+
+    Maintainer: Selma Wanna, slwanna@utexas.edu
+"""
+import random
+import rospy
+
+def create_patterns():
+    """
+        Function: create_patterns
+        Description: fills out all possible pattern solutions
+        Input: none
+        Output: all possible solutions
+    """
+    possible_patterns = []
+    possible_patterns.append("rrgygy")
+    possible_patterns.append("rryygb")
+    possible_patterns.append("rybygg")
+    possible_patterns.append("rybyrb")
+    possible_patterns.append("yyyyrb")
+    possible_patterns.append("yyyygg")
+    possible_patterns.append("gbrrby")
+    possible_patterns.append("ggrrby")
+    possible_patterns.append("bbggry")
+    possible_patterns.append("bbggrb")
+    possible_patterns.append("bgbgby")
+    possible_patterns.append("bgbrby")
+    return possible_patterns
+
+def create_starter_patterns():
+    """
+        Function: create_starter_patterns
+        Description: list of blocks people begin with
+        Input: none
+        Output: list of starting blocks
+    """
+    starter_patterns = 'rgyb'
+    return starter_patterns
+
+def compare_pattern(current_pattern, possible_patterns):
+    """
+        Function: compare_pattern
+        Description: compares currently viewed pattern against possible solutions
+        Input: current_pattern (string), possible_patterns (list of strings)
+        Output: possible solutions  (list of strings)
+    """
+    if len(possible_patterns) <= 1:
+        rospy.loginfo('Pattern Known from compare_pattern')
+        return possible_patterns
+    end_index = len(possible_patterns)
+    new_possibilities = []
+    for i in range(0, end_index):
+        if possible_patterns[i].startswith(current_pattern):
+            new_possibilities.append(possible_patterns[i])
+    return new_possibilities
+
+def next_block(rand, possible_patterns, current_pattern, num_tries=None):
+    """
+        Function: next_block
+        Description: Chooses which block should be tried next
+        Input: possible_patterns (string array), current_pattern (string)
+        Output: block which should be tried next
+    """
+    if rand:
+        colors = ['r', 'y', 'g', 'b']
+        return random.choice(colors)
+    else:
+        num_blocks = len(current_pattern)
+        if num_tries is None:
+            first_possible_pattern = possible_patterns[0]
+            if num_blocks + 1 <= 6:
+                print first_possible_pattern[num_blocks]
+                return first_possible_pattern[num_blocks] # return next character
+            else:
+                print "puzzle complete!"
+                print "possible index out of bound"
+                rospy.logerr('Indexed out of bound in the next_block() function!!!')
+                return '0' # end of sequence. Something went wrong
+        else:
+            if num_blocks + 1 <= 6:
+                print possible_patterns[num_tries % len(possible_patterns)][num_blocks]
+                return possible_patterns[num_tries % len(possible_patterns)][num_blocks]
+            else:
+                print "puzzle complete!"
+                print "possible index out of bound"
+                rospy.logerr('Indexed out of bound in the next_block() function!!!')
+                return '0' # end of sequence. Something went wrong
+
+def is_piece_available(color, starter_pieces):
+    """
+        Function: is_piece_available
+        Description: Checks to see if user has the next block
+        Input: next block color (string) and starter pieces (string)
+        Output: boolean to determine if robot needs to hand it over
+    """
+    if color in starter_pieces:
+        rospy.loginfo('User has needed piece: ' + color)
+        return False
+    else:
+        rospy.loginfo('Robot will handover piece: ' + color)
+        return True
+
+#def main():
+#    possible_patterns = []
+#    possible_patterns = create_patterns()
+#    current_pattern = 'bgbgby'
+#    possible_patterns = compare_pattern(current_pattern, possible_patterns)
+#    print possible_patterns
+#    print next_block(True, possible_patterns, current_pattern, 3)
+#if  __name__ == '__main__':
+#    main()
